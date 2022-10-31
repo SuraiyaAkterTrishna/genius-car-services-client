c6d8a92e431c98aa93d91af922557429991add64
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-10-31 14:38:30 +0600

    add carousel from react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
