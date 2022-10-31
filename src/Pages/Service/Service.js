6012ccf3fde23965cacda9f921562cf9c100f4d6
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-10-31 12:49:18 +0600

    services section done

--- src/Pages/Service/Service.js
+++ src/Pages/Service/Service.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-const Service = ({service}) => {
-    const {id, name, description, price} = service;
-    return (
-        <div>
-            <h1>This is service:{id}</h1>
-        </div>
-    );
+const Service = ({ service }) => {
+  const { id, img, name, description, price } = service;
+  return (
+    <Container>
+      <Card>
+        <Card.Img variant="top" src={img} />
+        <Card.Body>
+          <Card.Title>
+            <h4>{name}</h4>
+          </Card.Title>
+          <Card.Text>
+            <p>Price: ${price}</p>
+            <p>{description}</p>
+          </Card.Text>
+          <Button variant="primary">Book: {name}</Button>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 };
 
 export default Service;
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
-import Row from 'react-bootstrap/Row';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Service from "../Service/Service";
 
 const Services = () => {
@@ -11,15 +12,14 @@ const Services = () => {
       .then((data) => setServices(data));
   }, []);
   return (
-    <Container>
-      <h1 className="text-primary text-center">Our services</h1>
-      <Row>
-        {
-            services.map(service =><Service
-                key = {service.id}
-                service = {service}
-            ></Service>)
-        }
+    <Container fluid>
+      <h1 className="text-primary text-center text-uppercase my-5">Our Services</h1>
+      <Row className="gy-4">
+          {services.map((service) => (
+            <Col xs={12} md={6} lg={4} xl={4}>
+            <Service key={service.id} service={service}></Service>
+            </Col>
+          ))}
       </Row>
     </Container>
   );
