f529871c5a3dd3fd9ec2ccacf6e9d8bb9f815a56
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-10-31 15:07:31 +0600

    footer added in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Header from './Pages/Shared/Header/Header';
 import Services from './Pages/Services/Services';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/service' element={<Services></Services>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
