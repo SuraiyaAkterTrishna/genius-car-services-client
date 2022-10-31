f984fe30cc472566db914d5c92a373b0f707b275
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-10-31 15:12:07 +0600

    footer done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <footer>
-            <p>copyright @ {year}</p>
+        <footer className='text-center mt-5 bg-dark text-white'>
+            <p className='p-5'>copyright @ {year}</p>
         </footer>
     );
 };
