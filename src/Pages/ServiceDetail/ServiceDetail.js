7827251b93d48369d722bc1a13387bfe9f2ab1de
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-11-02 16:11:27 +0600

    show service detail, reading url params

---
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServiceDetail = () => {
+    return (
+        <div>
+            <h1>Welcome to detail:</h1>
+        </div>
+    );
+};
+
+export default ServiceDetail;
