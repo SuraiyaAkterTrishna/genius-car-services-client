e580443ed74876059e99c1a260048ee26c9b74c2
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-11-02 16:20:47 +0600

    working on serviceDetail component

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ServiceDetail = () => {
     return (
         <div>
-            <h1>Welcome to detail</h1>
+            <h1>Welcome to detail:</h1>
         </div>
     );
 };
