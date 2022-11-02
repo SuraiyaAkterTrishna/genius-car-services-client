8ba0a128f910879c81ea71694d0b60e8cb8ed7d0
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-11-02 16:13:24 +0600

    set dynamic route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './Pages/Shared/Header/Header';
 import Services from './Pages/Services/Services';
 import Footer from './Pages/Shared/Footer/Footer';
 import Experts from './Pages/Experts/Experts';
+import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/service' element={<Services></Services>}></Route>
+        <Route path='/service/:id' element={<ServiceDetail></ServiceDetail>}></Route>
         <Route path='/expert' element={<Experts></Experts>}></Route>
       </Routes>
       <Footer></Footer>
