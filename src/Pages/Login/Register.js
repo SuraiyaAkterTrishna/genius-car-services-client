72f6ca4e1c1a4a26c29917cc8e656ae308230318
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-11-08 12:45:40 +0600

    login register useNavigate useRef hooks done

--- src/Pages/Login/Register.js
+++ src/Pages/Login/Register.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css';
 
 const Register = () => {
+    const navigate = useNavigate();
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+    const handleRegister = event => {
+        event.preventDefault();
+        console.log(event.target.email.value);
+    }
     return (
-        <div>
-            <h1>This is register page</h1>
+        <div className='register-form'>
+            <h1 className='text-primary' style={{textAlign: 'center'}}>Register Now</h1>
+            <form onSubmit={handleRegister} action="">
+                <input type="text" name="name" placeholder='Your Name' />
+                <input type="email" name="email" id="" placeholder='Your Email'/>
+                <input type="password" name="password" id="" placeholder='Your Password' />
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account? <Link to='/login' className="text-danger text-decoration-none" onClick={navigateLogin}>Login Now.</Link> </p>
         </div>
     );
 };
