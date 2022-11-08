2caea59f2f81f321827e137cc4f328a57636fe3b
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-11-08 13:01:30 +0600

    hide environment variable

--- firebase.init.js
+++ firebase.init.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: "AIzaSyDVFBGVvm373RVYqzA_6aMfHqIJZMfsUZs",
-  authDomain: "genius-car-services-c3c39.firebaseapp.com",
-  projectId: "genius-car-services-c3c39",
-  storageBucket: "genius-car-services-c3c39.appspot.com",
-  messagingSenderId: "700930816253",
-  appId: "1:700930816253:web:18efd4859aa0ff23c22694"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
