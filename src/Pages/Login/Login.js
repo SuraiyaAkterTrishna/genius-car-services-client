d7951dbe7e5b2d2d4f60c8aff6c2ac4a686b12d8
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-11-08 15:33:35 +0600

    Start email password based firebase authentication

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -28,7 +28,6 @@ const Login = () => {
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
