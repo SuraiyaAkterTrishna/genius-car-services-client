688d72fb99e7e6bfef4c0b3f54c301b3dc996cbe
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-11-09 11:47:05 +0600

    google login

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -3,10 +3,13 @@ import React from 'react';
 const SocialLogin = () => {
     return (
         <div>
+            <div className='d-flex align-items-center'>
+                <div style={{height: "1px"}} className='bg-primary w-50'></div>
+                <p className='mt-2 px-2'>or</p>
+                <div style={{height: "1px"}} className='bg-primary w-50'></div>
+            </div>
             <div>
-                <div></div>
-                <p>or</p>
-                <div></div>
+                <button className='btn btn-primary w-50'>Google Sign in</button>
             </div>
         </div>
     );
