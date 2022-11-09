70c0a25ec4868e5aa000ca49804ab2f9d7259dcf
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-11-09 11:24:58 +0600

    organized login form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -5,55 +5,63 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 
 const Login = () => {
-  const emailRef = useRef('');
-  const passwordRef = useRef('');
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
   const navigate = useNavigate();
   const location = useLocation();
   let from = location.state?.from?.pathname || "/";
 
-  const [
-    signInWithEmailAndPassword,
-    user,
-    loading,
-    error,
-  ] = useSignInWithEmailAndPassword(auth);
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
-    
+
     signInWithEmailAndPassword(email, password);
-  }
-  if(user){
+  };
+  if (user) {
     navigate(from, { replace: true });
   }
   const navigateRegister = () => {
-    navigate('/register');
-  }
+    navigate("/register");
+  };
   return (
     <div className="container w-50 mx-auto">
       <h1 className="text-primary mt-4">Please Login.</h1>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox"></Form.Group>
         <Button onClick={handleSubmit} variant="primary" type="submit">
           Submit
         </Button>
       </Form>
-      <p>New to Genius Car? <Link to='/register' className="text-danger text-decoration-none" onClick={navigateRegister}>Please Register.</Link> </p>
+      <p>
+        New to Genius Car?{" "}
+        <Link
+          to="/register"
+          className="text-danger text-decoration-none"
+          onClick={navigateRegister}
+        >
+          Please Register.
+        </Link>{" "}
+      </p>
     </div>
   );
 };
