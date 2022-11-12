1544600124c59da2c07d2ae83c69e2a018a93560
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-11-12 11:17:40 +0600

    conditional rendering

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,23 +1,20 @@
 import React from "react";
 import google from "../../../images/Social logo/google.png";
-import facebook from "../../../images/Social logo/facebook.png";
-import github from "../../../images/Social logo/github.png";
 import auth from "../../../firebase.init";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
-    const navigate = useNavigate();
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  const [signInWithGoogle, user, error] = useSignInWithGoogle(auth);
   let errorElement;
-  if (error) {
-    errorElement = <div>
-        <p>Error: {error.message}</p>
-      </div>
+  if (error ) {
+    errorElement = ( <p>Error: {error?.message}</p>
+    );
   }
-  
+
   if (user) {
-    navigate('/home');
+    navigate("/home");
   }
   return (
     <div>
@@ -28,17 +25,12 @@ const SocialLogin = () => {
       </div>
       {errorElement}
       <div>
-        <button onClick={() => signInWithGoogle()} className="btn btn-info text-white w-35 me-2 my-2">
+        <button
+          onClick={() => signInWithGoogle()}
+          className="btn btn-info text-white w-100 me-2 my-2"
+        >
           <img style={{ width: "25px" }} src={google} alt="image1" />
           <span className="px-2">Google</span>
-        </button>
-        <button className="btn btn-info text-white w-35 me-2 my-2">
-          <img style={{ width: "25px" }} src={facebook} alt="image2" />
-          <span className="px-2">Facebook</span>
-        </button>
-        <button className="btn btn-info text-white w-35 my-2">
-          <img style={{ width: "25px" }} src={github} alt="image3" />
-          <span className="px-2">GitHub</span>
         </button>
       </div>
     </div>
